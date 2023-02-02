101f20231a64aeb26706477983f0b66ad9d81f71
Thunyaporn Tangcharoenchitkul <6210612641@student.tu.ac.th>
2023-02-02 11:11:25 +0700

    1st commit

---
+++ website/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+from datetime import date, datetime
+from django.contrib import messages
+
+def index(request):
+     
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def todolist(request):
+    if request.method == "POST":
+        form = ListModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            print("Error", form.errors)
+    form = ListModelForm()
+    show = ListModel.objects.all()
+    return render(request, 'todolist.html', {'form':form})
+
+
+
+# def todolist(request):
+#     form = todoForm()
+#     date_now = datetime.now()
+#     if request.method == 'POST':
+#         todo_input = request.POST.get("todo")
+#         date_input = request.POST.get("date_input")
+#         if datetime(int(date_input[:4]), int(date_input[5:7]), int(date_input[8:]),) <= date_now:
+#             messages.add_message(request, messages.SUCCESS, f"ไม่สามารถเพิ่มรายการได้ กรุณาเลือกวันให้ถูกต้อง")   #กรณีเลือกวันที่ผ่านมาแล้ว
+#             return redirect("doctors:appointment", pk)
+#         appointment = Appointment.objects.create(
+#             Doctor_id=doctor,
+#             todo=todo_input
+#         )
+#         appointment.dateapp = date_input
+#         appointment.save()
+#         return redirect("doctors:profile")
+
+#     context = {"form": form, "doctor": doctor}
+#     return render(request, "doctors/appointment_patient.html", context)
